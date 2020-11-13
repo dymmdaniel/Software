81c3adb07f98db225a2d8caacbc435bc26293ad6
danie <danie@AsusDaniel>
2020-11-13 18:21:55 -0500

    Version 1

---
+++ src/modelo/Ingreso.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author danie
+ */
+public class Ingreso {
+    private String user;
+    private String contra;
+    public boolean validar_usuario(String user,String contra){
+        return true;
+    }
+}
